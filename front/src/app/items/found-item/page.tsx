d5cd4746cfaa10378a61ab8f6a5f54af5433bbee
kth4778 <cnblue123429@gmail.com>
2025-08-15 11:44:33 +0900

    refactor: 분실물 신고 등록 오류3

---
+++ front/src/app/items/found-item/page.tsx
@@ -0,0 +1,8 @@
+export default function FoundItemsListPage() {
+  return (
+    <div>
+      <h1>Found Items List Page</h1>
+      <p>This page will display a list of found items.</p>
+    </div>
+  );
+}
