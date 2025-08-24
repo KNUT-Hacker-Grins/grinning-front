50ac04fec854bf1c6481e5fe4fd6acb397915160
somedding <tycoontom42@gmail.com>
2025-08-24 09:57:05 +0900

    new page

--- front/src/app/found-item/page.tsx
+++ front/src/app/found-item/page.tsx
@@ -117,7 +117,7 @@ export default function FoundPage() {
       <MainHeader isAuthenticated={isAuthenticated} authLoading={authLoading} user={user} />
       <div className="px-4 py-6">
         {/* 카테고리 필터 */}
-        <div className="flex gap-[15px] mb-[13px] pl-[23px] overflow-x-auto">
+        <div className="flex gap-[15px] mb-[13px] pl-[23px] overflow-x-auto scrollbar-hide">
           {categories.map((category) => (
             <button
               key={category}
